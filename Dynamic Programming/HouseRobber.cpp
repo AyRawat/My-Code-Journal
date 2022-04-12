63a128a8a1cce39d542c8efdac83195de137cb26
Aayush Rawat <aayushrawat20@gmail.com>
2022-04-12 17:04:17 +0530

    BackTracking-NumPAd_StringPerm

--- Dynamic Programming/HouseRobber.cpp
+++ Dynamic Programming/HouseRobber.cpp
@@ -1,3 +1,17 @@
+/*
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+*/
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -21,6 +35,7 @@ int houseRobber(int index, vector<int> &money){
     return max(money[index]+houseRobber(index+2,money) , houseRobber(index+1, money));
 }
 //DP
+
 int houseRobberDP(vector<int> &money){
 if(money.size() == 0) return 0;
 if(money.size() == 1) return money[0];
