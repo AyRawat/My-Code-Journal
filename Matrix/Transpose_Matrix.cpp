8224e2001f1f2d15aef60fc556b66a5e1b5a0d84
Aayush Rawat <aayushrawat20@gmail.com>
2022-06-03 01:10:49 +0530

    Transpose_MAtrix

---
+++ Matrix/Transpose_Matrix.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+ vector<vector<int>> transposed(n,vector<int> (m,0));
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+           transposed[j][i] = matrix[i][j];    
+        }
+    }
+    return transposed;
+    }
+
+
+
+int main(){
+vector<vector<int> > matrix = {{1,2,3},{4,5,6},{7,8,9}};
+    return 0;
+}
