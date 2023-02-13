eba5b9f21371a02262e7721e1fde9dfba41f5549
AyRawat <aayushrawat20@gmail.com>
2023-02-13 16:42:29 +0530

    Added Optimal Approach for Deleting Nth node from back in Linkedlist

--- LinkedList/RemoveNthNodeFromEnd.cpp
+++ LinkedList/RemoveNthNodeFromEnd.cpp
@@ -6,7 +6,23 @@ using namespace std;
       ListNode(int x) : val(x), next(NULL) {}
   };
 
-   
+//Optimal Approach
+    ListNode* removeNthFromEndOptimal(ListNode* head, int n) {
+        ListNode* start = new ListNode();
+        start->next = head;
+        ListNode* fast = start;
+        ListNode* slow = start;
+        for(int i=1;i<=n;i++) {
+            fast = fast->next;
+        }
+        while(fast->next!=NULL){
+            fast =fast->next;
+            slow =slow->next;
+        }
+        slow->next = slow->next->next;
+        return start->next;
+    }
+   //Naive Approach
    ListNode* removeNthFromEnd(ListNode* head, int n) {
         if(head == NULL) return head;
         int count = 0;
