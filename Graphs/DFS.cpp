b197024a18a8639f2809c03b0814d743f8aa8f06
AyusH Rawat <ayushrawat@AyusHs-MacBook-Pro.local>
2023-03-16 13:13:38 +0530

    Added DFS Graph

--- Graphs/DFS.cpp
+++ Graphs/DFS.cpp
@@ -1,4 +1,4 @@
-    void dfs(int V, vector<int> adj[] , vector<int> &visited , vector<int> &ls){
+void dfs(int V, vector<int> adj[] , vector<int> &visited , vector<int> &ls){
         if(!visited[V]){
             visited[V] = 1;
             ls.push_back(V);
