917eef006a1bc108f2f19855efcc6e0f2ceeec15
AyRawat <aayushrawat20@gmail.com>
2024-04-08 07:58:27 +0530

    added comments

--- Graphs/Shortest-Paths/FloydWarshall.cpp
+++ Graphs/Shortest-Paths/FloydWarshall.cpp
@@ -2,11 +2,14 @@
 	    // Code here
 	    int m = matrix.size();
 	    vector<vector<int>> adj = matrix;
+      //For comparison purposes we are replacing -1 to 1e9.
+      //also if i == j , that means source == distance , therefore the distance will be 0.
 	    for(int i=0;i<m;i++){
 	        for(int j=0;j<m;j++){
 	            if(matrix[i][j] == -1) adj[i][j] = 1e9;
 	            if(i == j) adj[i][j] = 0;
 	        }}
+      // We measure all the distances i to j via k. Hence, [i][k] to [k][j]
 	    for(int k=0;k<m;k++){
 	        for(int i=0;i<m;i++){
 	            for(int j=0;j<m;j++){
@@ -15,6 +18,7 @@
 	            }
 	        }
 	    }
+      //Reverting our all changes back.
 	    for(int i=0;i<m;i++){
 	        for(int j=0;j<m;j++){
 	            if(adj[i][j] == 1e9) adj[i][j] = -1;
