dc8f023d839c0d2840612e8847469017b4d14f3c
AyRawat <aayushrawat20@gmail.com>
2024-04-08 10:37:36 +0530

    fixed disjoint set

--- Graphs/MinimumSpanningTree/NumberOfProvinces.cpp
+++ Graphs/MinimumSpanningTree/NumberOfProvinces.cpp
@@ -3,55 +3,55 @@
 
 #include<bits/stdc++.h>
 class DisjointSet {
-  vector<int> rank, parent, size;
-  public: 
-      DisjointSet(int n){
-        rank.resize(n+1, 0);
-        parent.resize(n+1);
-        size.resize(n+1,1);
-        iota(parent.begin(), parent.end(), 1);
-      }
-      //Find the ultimare parent
-      int findUPar(int node){
-        if(node == parent[node]) return node;
-        //Path Compression
-        return parent[node] = findUPar(parent[node]);
-      }
-      void unionByRank(int u, int v){
-        int uParentU = findParent(u);
-        int uParentV = findParent(v);
-        //If they belong to the same parent;
-        if(uParentU == uParentV) return;
-
-        int uRank = rank[uParentU];
-        int vRank = rank[uParentV];
-      //If the rank is either small or large. Just switch up the parents. And Rank will remain same
-      //as we are only adding to the same level.
-      if(uRank < vRank){
-        parent[uParentU] = uParent[V]; 
-      }else if(uRank > vRank){
-        parent[uParentV] = uParentU;
-      }else{
-        //If the rank is same, then you need to increase it after the union of u to v.
-        parent[uParentV] = uParentU;
-        rank[uParentU]++;
-      }
-      void unionBySize(int u, int v){
-        int uParentU = findParent(u);
-        int uParentV = findParent(v);
-        if(uParentU == uParentV) return;
-
-        int uSize = size[uParentU];
-        int vSize = size[uParentV];
-        if(uSize < vSize){
-          parent[uParentU] = uParentV;
-          size[uParentV] += size[uParentU];
-        }else{
-          parent[uParentV] = uParentU;
-          size[uParentU]+= size[uParentU];
+public:
+    vector<int> rank, parent, size;
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
         }
-      }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
 };
+
 int numberOfProvinces(vector<vector<int>>adj, int V){
   int numOfProvinces = 0;
   DisjointSet d_set(V);
